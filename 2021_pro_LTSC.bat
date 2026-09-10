981fb96a49d521710c4ecd9aeb9c069aa329f5f1
Nuwan JK <234105831+nuwan-jk@users.noreply.github.com>
2026-09-10 18:42:33 +0530

    Refactor admin check for script elevation

--- 2021_pro_LTSC.bat
+++ 2021_pro_LTSC.bat
@@ -1,15 +1,21 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: [1] SIMPLE ADMIN CHECK
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    color 0C
-    echo [FATAL ERROR] Please right-click this script and choose "Run as administrator"!
-    pause
-    exit /b
-)
+:: [1] ROBUST AUTO-ADMIN CHECK & ELEVATION
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
 
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 pushd "%CD%"
 CD /D "%~dp0"
 
