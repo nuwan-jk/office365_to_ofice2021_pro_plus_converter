c7f0bc014cd87ba4c72ec097ac39162b27d8c501
Nuwan JK <234105831+nuwan-jk@users.noreply.github.com>
2026-09-10 22:19:12 +0530

    Update 2021LTSC.bat

--- 2021LTSC.bat
+++ 2021LTSC.bat
@@ -4,18 +4,19 @@ setlocal EnableDelayedExpansion
 pushd "%CD%"
 CD /D "%~dp0"
 
-title Office 2021 LTSC Enterprise Setup
+title Office 2021 LTSC Local Setup
 color 0B
 echo ==================================================
-echo    OFFICE 2021 LTSC - AUTO SETUP & ACTIVATION
+echo    OFFICE 2021 LTSC - SECURE LOCAL ACTIVATOR
 echo ==================================================
 echo.
 
-:: [1] ADMIN PRIVILEGES CHECK & ELEVATION
+:: [1] ADMIN CHECK
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo [*] Requesting Administrator privileges...
-    powershell -NoProfile -Command "Start-Process '%~f0' -Verb RunAs"
+    color 0C
+    echo [ERROR] Please right-click this script and select "Run as administrator"!
+    pause
     exit
 )
 
@@ -28,7 +29,7 @@ for %%a in (winword excel powerpnt outlook OfficeClickToRun) do (
 )
 
 :: [3] DETECT OFFICE & GUID
-echo [*] Step 2: Detecting Office Installation & PackageGUID...
+echo [*] Step 2: Detecting Office Installation...
 set "_InstallRoot="
 set "_GUID="
 for /f "skip=2 tokens=2*" %%a in ('reg query "HKLM\SOFTWARE\Microsoft\Office\ClickToRun" /v InstallPath 2^>nul') do (set "_InstallRoot=%%b\root")
@@ -56,8 +57,8 @@ if not exist "%OSPP%" (
     exit
 )
 
-:: [4] WIPE OLD LICENSES VIA REGISTRY
-echo [*] Step 3: Purging old Office licenses and cache...
+:: [4] WIPE OLD LICENSES
+echo [*] Step 3: Purging old Office licenses...
 reg delete "HKLM\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" /v ProductReleaseIds /f >nul 2>&1
 reg delete "HKCU\Software\Microsoft\Office\16.0\Common\Licensing" /f >nul 2>&1
 reg delete "HKCU\Software\Microsoft\Office\16.0\Registration" /f >nul 2>&1
@@ -69,9 +70,7 @@ net start ClickToRunSvc >nul 2>&1
 echo [*] Step 4: Forcing LTSC 2021 Volume Conversion...
 if not "%_GUID%"=="" (
     if exist "%Integrator%" (
-        setlocal DisableDelayedExpansion
         "%Integrator%" /I /License PRIDName=ProPlus2021Volume.16 PackageGUID="%_GUID%" PackageRoot="%_InstallRoot%" >nul 2>&1
-        endlocal
     )
 )
 
@@ -82,24 +81,16 @@ for /f "delims=" %%x in ('dir /b "%LicensesPath%\ProPlus2021*.xrm-ms" 2^>nul') d
     cscript //nologo "%OSPP%" /inslic:"%LicensesPath%\%%x" >nul 2>&1
 )
 
-:: [7] EMBEDDED POWERSHELL OHOOK DEPLOYMENT & ACTIVATION
-echo [*] Step 5: Applying Permanent Activation Engine...
-powershell -NoProfile -Command ^
-"$arch = if ([Environment]::Is64BitOperatingSystem) { 'x64' } else { 'x86' };" ^
-"$C2RPath = if (Test-Path \"$env:ProgramFiles\Common Files\Microsoft Shared\ClickToRun\") { \"$env:ProgramFiles\Common Files\Microsoft Shared\ClickToRun\" } else { \"${env:ProgramFiles(x86)}\Common Files\Microsoft Shared\ClickToRun\" };" ^
-"$url = \"https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/main/MAS/All-In-Version/Files/Ohook/$arch/sppc.dll\";" ^
-"try { Invoke-WebRequest -Uri $url -OutFile \"$C2RPath\sppc.dll\" -UseBasicParsing } catch {}" >nul 2>&1
+:: [7] DEPLOY OHOOK ENGINE
+echo [*] Step 5: Deploying Activation Engine...
+set "C2RPath=%ProgramFiles%\Common Files\Microsoft Shared\ClickToRun"
+if not exist "%C2RPath%" set "C2RPath=%ProgramFiles(x86)%\Common Files\Microsoft Shared\ClickToRun"
 
-:: [8] SET DEFAULT GVLK KEY FOR IMMEDIATE ACTIVE STATE
+powershell -NoProfile -Command "try { Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/main/MAS/All-In-Version/Files/Ohook/x64/sppc.dll' -OutFile '%C2RPath%\sppc.dll' -UseBasicParsing } catch {}" >nul 2>&1
+
+:: [8] SET KEY FOR ACTIVE STATE
 echo [*] Step 6: Setting Volume Activation Key...
 cscript //nologo "%OSPP%" /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH >nul 2>&1
-
-:: [9] APPLYING OFFICE POLICY TWEAKS
-echo [*] Step 7: Applying Office Policy Tweaks...
-reg add "HKCU\Software\Microsoft\Office\16.0\Common\SignIn" /v SignInOptions /t REG_DWORD /d 0 /f >nul 2>&1
-reg add "HKCU\Software\Microsoft\Office\16.0\Common\Internet" /v UseOnlineContent /t REG_DWORD /d 2 /f >nul 2>&1
-reg add "HKLM\SOFTWARE\Policies\Microsoft\office\16.0\common\licensing" /v SubscriptionValidationToggle /t REG_DWORD /d 1 /f >nul 2>&1
-reg add "HKLM\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" /v SharedComputerLicensing /t REG_SZ /d "0" /f >nul 2>&1
 
 color 0A
 echo.
